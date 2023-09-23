d1bab56bbe4160951e5e546cf80069b4d32f3710
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-09-23 16:22:45 +0300

    goit-react-hw-04-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,98 +1,73 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Forms } from './Phonebook/Form/Form';
 import { ContactList } from './Phonebook/Contactlist/ContactList';
 import { Filter } from './Phonebook/Filter/Filter';
-import Notiflix from 'notiflix';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Wrap, Titel } from './App.style';
-
-const initialValues = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-
-  filter: '',
+import { useState, useEffect, useMemo } from 'react';
+import { toast } from 'react-hot-toast';
+const userContacts = () => {
+  const saveCont = localStorage.getItem('contacts');
+  if (saveCont !== null) {
+    return JSON.parse(saveCont);
+  } else {
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  }
 };
+export const App = () => {
+  const [contacts, setContacts] = useState(userContacts);
+  const [filter, setFilter] = useState('');
 
-export class App extends Component {
-  state = { ...initialValues };
-  componentDidMount = () => {
-    const saveCont = localStorage.getItem('contacts');
-    if (saveCont !== null) {
-      this.setState({
-        contacts: JSON.parse(saveCont),
-      });
-    }
-  };
-
-  componentDidUpdate = (prevProps, prevState) => {
-    const isNewContact = prevState.contacts !== this.state.contacts;
-
-    if (isNewContact)
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  };
-  submitContact = (values, { resetForm }) => {
+  const submitContact = (values, { resetForm }) => {
+    const { name, number } = values;
+    const newContacts = {
+      name: name,
+      number: number,
+      id: nanoid(),
+    };
     if (
-      this.state.contacts.filter(
+      contacts.find(
         contact =>
-          contact.name.toLowerCase().trim() ===
-            values.name.toLowerCase().trim() ||
-          contact.number.trim() === values.number.trim()
-      ).length
+          contact.name.toLowerCase() === newContacts.name.toLowerCase() ||
+          contact.number.toLowerCase() === newContacts.number.toLowerCase()
+      )
     ) {
       return (
-        Notiflix.Notify.failure(
-          ` Contact ${values.name} was already entered earlier`
-        ),
-        resetForm()
+        toast.error(` Contact ${name} was already entered earlier`), resetForm()
       );
     } else {
-      const newContacts = {
-        name: values.name,
-        number: values.number,
-        id: nanoid(),
-      };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContacts],
-      }));
+      setContacts(prevState => [...prevState, newContacts]);
       resetForm();
     }
   };
-  onDeletContacts = id => {
-    this.setState(prevStete => ({
-      contacts: prevStete.contacts.filter(contacts => contacts.id !== id),
-    }));
+  const onDeletContacts = id => {
+    setContacts(contacts.filter(contacts => contacts.id !== id));
   };
 
-  filterForm = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const filterForm = e => {
+    setFilter(e.currentTarget.value);
   };
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
-    const normalizFilter = filter.toLowerCase();
+  const filterContacts = useMemo(() => {
     return contacts.filter(contacts =>
-      contacts.name.toLowerCase().includes(normalizFilter)
+      contacts.name.toLowerCase().includes(filter.toLowerCase())
     );
-  };
-  render() {
-    const filterContacts = this.filterContacts();
-
-    return (
-      <Wrap>
-        <Titel>Phonebook</Titel>
-        <Forms submitContact={this.submitContact} />
-        <Titel>Contacts</Titel>
-        <Filter filter={this.state.filter} filterForm={this.filterForm} />
-        <ContactList
-          contacts={filterContacts}
-          onDelete={this.onDeletContacts}
-        />
-        <GlobalStyle />
-      </Wrap>
-    );
-  }
-}
+  }, [filter, contacts]);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  return (
+    <Wrap>
+      <Titel>Phonebook</Titel>
+      <Forms submitContact={submitContact} />
+      <Titel>Contacts</Titel>
+      <Filter filter={filter} filterForm={filterForm} />
+      <ContactList contacts={filterContacts} onDelete={onDeletContacts} />
+      <GlobalStyle />
+    </Wrap>
+  );
+};
